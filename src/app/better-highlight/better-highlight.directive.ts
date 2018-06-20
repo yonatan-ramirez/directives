3929d4609b067829da2c965f43b71615dfbc0be8
Yonatan <yonatan.ramirez557@gmail.com>
2018-06-20 15:07:40 -0700

    useing HostBinding for style.backgrounColor of host element to change style on an event

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, ElementRef, OnInit, HostListener } from '@angular/core';
+import { Directive, Renderer2, ElementRef, OnInit, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -8,20 +8,41 @@ export class BetterHighlightDirective implements OnInit {
   constructor(private renderer: Renderer2,
   private elementRef: ElementRef) { }
 
+  // HostListener begining!-----
   ngOnInit() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
     // forth argument is optional. We can set !important in that argument
   }
+
+//   // Host listener decorator is added to a method we want to execute on an event
+//   // when the moust enters (hovers over) the listener it turns blue!
+//   @HostListener('mouseenter') mouseEnter() {
+// // listening for mouseenter event on host
+// this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+//   }
+// // when the mouse pointer no longer hovers over it turns transparent
+//   @HostListener('mouseleave') mouseOut() {
+//     // listening for mouse leave event on host
+//     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+//   }
+// HostListener End--------
+
+// HostBinding Begining -------------
+                                                // backgroundColor: string = 'transparent';
+    // tslint:disable-next-line:member-ordering
+    @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+    // Bind to host property using HostBinding!
+    // define in string
+    // camelCase property name because we are accessing DOM property not CSS
 
   // Host listener decorator is added to a method we want to execute on an event
   // when the moust enters (hovers over) the listener it turns blue!
   @HostListener('mouseenter') mouseEnter() {
-// listening for mouseenter event on host
-this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue';
   }
 // when the mouse pointer no longer hovers over it turns transparent
   @HostListener('mouseleave') mouseOut() {
-    // listening for mouse leave event on host
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
+  // HostBinding End! -------------------
 }
