681047cd3400cdc62733a351e0be450aeb3f5766
Yonatan <yonatan.ramirez557@gmail.com>
2018-06-20 14:49:11 -0700

    listening to host event HostListener to apply styles

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, ElementRef, OnInit } from '@angular/core';
+import { Directive, Renderer2, ElementRef, OnInit, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -11,5 +11,17 @@ export class BetterHighlightDirective implements OnInit {
   ngOnInit() {
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
     // forth argument is optional. We can set !important in that argument
+  }
+
+  // Host listener decorator is added to a method we want to execute on an event
+  // when the moust enters (hovers over) the listener it turns blue!
+  @HostListener('mouseenter') mouseEnter() {
+// listening for mouseenter event on host
+this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+  }
+// when the mouse pointer no longer hovers over it turns transparent
+  @HostListener('mouseleave') mouseOut() {
+    // listening for mouse leave event on host
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
   }
 }
