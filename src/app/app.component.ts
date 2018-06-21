ecce9e627e6282c4698cb5f6682b55605c6a235c
Yonatan <yonatan.ramirez557@gmail.com>
2018-06-20 17:39:11 -0700

    understanding switch & ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  numbers = [1, 2, 3, 4, 5];
+export class AppComponent implements OnInit {
+  // numbers = [1, 2, 3, 4, 5];
   oddNumbers = [ 1, 3 , 5];
   evenNumbers = [2, 4];
   onlyOdd = false;
+  // tslint:disable-next-line:no-inferrable-types
+  value: number = 10;
+
+  ngOnInit() {
+    // if (this.value === 5) {
+    //   console.log('inside first if');
+    // } else if (this.value === 10) {
+    //   console.log('inside second if');
+    // } else if (this.value === 100) {
+    //   console.log('inside third if');
+    // } else {
+    //   console.log('none');
+    // }
+    switch (this.value) {
+      case 5:
+        console.log('case 5');
+        break;
+        case 10:
+          console.log('case 10');
+          break;
+          case 100:
+            console.log('case 100');
+            break;
+            default:
+             console.log('none');
+    }
+  }
 
 }
